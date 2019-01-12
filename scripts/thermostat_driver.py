3e5d0ccb446cbf6d300e3df8e13c481031bda4fc
jgines <jonathangines@hotmail.com>
2019-01-12 16:12:28 +0100

    ros node

--- scripts/thermostat_driver.py
+++ scripts/thermostat_driver.py
@@ -4,30 +4,23 @@ import time                #Importamos time para poder usar time.sleep
 import rospy
 from geometry_msgs.msg import Twist
 
-def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + " %d received", data.data
+pin = 0
 
-def step():
+def callback(data):
+    global pin
+    wave = (10 / (math.pi / data.angular.x)) + 0.5
+    pin.ChangeDutyCycle(wave)   #Enviamos un pulso del 10.5% para girar el servo hacia la izquierda
 
 if __name__ == '__main__':
+    global pin
     try:
-         rospy.init_node('thermostat_driver', anonymous=False)
-         rospy.Subscriber('/thermostat/turn', Twist, callback)
-         rate = rospy.Rate(10)
-
-         GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
-         GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
-         p = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
-         p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
-
-         while not rospy.is_shutdown():
-            p.ChangeDutyCycle(10.5)    #Enviamos un pulso del 10.5% para girar el servo hacia la izquierda
-            time.sleep(0.5)           #pausa de medio segundo
-            p.ChangeDutyCycle(0.5)   #Enviamos un pulso del 0.5% para girar el servo hacia la derecha
-            time.sleep(0.5)           #pausa de medio segundo
-            p.ChangeDutyCycle(5.5)    #Enviamos un pulso del 5.5% para centrar el servo de nuevo
-            time.sleep(0.5)           #pausa de medio segundo
-            #rate.sleep()
+        rospy.init_node('thermostat_driver', anonymous=False)
+        rospy.Subscriber('/thermostat/turn', Twist, callback)
+        GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
+        GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
+        pin = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
+        pin.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
+        rospy.spin()
 
     except rospy.ROSInterruptException:         #Si el usuario pulsa CONTROL+C entonces...
         p.stop()                      #Detenemos el servo
