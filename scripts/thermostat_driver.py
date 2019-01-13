e89d50d351231134d190f5b1b0353ceef978c835
jginesclavero <jginesclavero@gmail.com>
2019-01-13 12:31:25 +0000

    Work work work work work

--- scripts/thermostat_driver.py
+++ scripts/thermostat_driver.py
@@ -5,24 +5,23 @@ import rospy
 import math
 from geometry_msgs.msg import Twist
 
-pin = 0
-
 def callback(data):
-    global pin
+    GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
+    GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
+    pin = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
+    pin.start(0)               #Enviamos un pulso del 7.5% para centrar el servo
+    if data.angular.x == 0.0:
+        data.angular.x = 0.1
     wave = (10 / (math.pi / data.angular.x)) + 0.5
     pin.ChangeDutyCycle(wave)   #Enviamos un pulso del 10.5% para girar el servo hacia la izquierda
+    time.sleep(0.5)
+    pin.stop()                      #Detenemos el servo
+    GPIO.cleanup()
 
 if __name__ == '__main__':
-    GPIO.cleanup()
-    global pin
     try:
         rospy.init_node('thermostat_driver', anonymous=False)
         rospy.Subscriber('/thermostat/turn', Twist, callback)
-        GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
-        GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
-        pin = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
-        #pin.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
         rospy.spin()
     except rospy.ROSInterruptException:         #Si el usuario pulsa CONTROL+C entonces...
-        p.stop()                      #Detenemos el servo
-        GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el script
+        pass
