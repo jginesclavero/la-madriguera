19c7fe0b91001c3e92ba90dc030d7b544f18a052
jginesclavero <jginesclavero@gmail.com>
2019-01-13 12:08:39 +0000

    minior changes form rb

--- scripts/thermostat_driver.py
+++ scripts/thermostat_driver.py
@@ -2,6 +2,7 @@
 import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import time                #Importamos time para poder usar time.sleep
 import rospy
+import math
 from geometry_msgs.msg import Twist
 
 pin = 0
@@ -12,14 +13,15 @@ def callback(data):
     pin.ChangeDutyCycle(wave)   #Enviamos un pulso del 10.5% para girar el servo hacia la izquierda
 
 if __name__ == '__main__':
-    global pin
+  GPIO.cleanup()
+  global pin
     try:
         rospy.init_node('thermostat_driver', anonymous=False)
         rospy.Subscriber('/thermostat/turn', Twist, callback)
         GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
         GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
         pin = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
-        pin.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
+        #pin.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
         rospy.spin()
 
     except rospy.ROSInterruptException:         #Si el usuario pulsa CONTROL+C entonces...
