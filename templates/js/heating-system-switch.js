fd690f5909ad6b9bc6aa9f44f5bf555536552b0a
jginesclavero <jginesclavero@gmail.com>
2019-11-21 19:42:32 +0100

    test connection

--- templates/js/heating-system-switch.js
+++ templates/js/heating-system-switch.js
@@ -1,8 +1,9 @@
 
 function check_switch_status(){
-  $.getJSON('https://la-madriguera-iot.herokuapp.com/heating-system/getStatus', function(data) {
-    document.getElementById("switch").checked = data[0].status;
-  });
+  //$.getJSON('https://la-madriguera-iot.herokuapp.com/heating-system/getStatus', function(data) {
+  //  document.getElementById("switch").checked = data[0].status;
+  //});
+  document.getElementById("switch").checked = false;
 }
 
 window.onload = check_switch_status();
