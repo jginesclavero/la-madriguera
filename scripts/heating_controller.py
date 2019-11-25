ea251ef6c798b81fbd51c13925fc3155881f6afc
jginesclavero <jginesclavero@gmail.com>
2019-11-25 18:04:18 +0100

    Add secure power off at midnight

--- scripts/heating_controller.py
+++ scripts/heating_controller.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
-import time               
+import time
+import datetime               
 import rospy
 from std_srvs.srv import SetBool,SetBoolRequest
 import requests
 
+sleeping = False
+
+def sleeping_mode():
+  global sleeping
+  if datetime.datetime.now().time().hour == 23 and not sleeping:
+    url = 'http://la-madriguera-iot.herokuapp.com/heating-system/setStatus?status=0'
+    requests.get(url=url)
+    sleeping = True
+  else:
+    sleeping = False
+
 def update_heating_status():
+  sleeping_mode()
   try:
     status = False
     url = 'http://la-madriguera-iot.herokuapp.com/heating-system/getStatus'
@@ -12,8 +25,8 @@ def update_heating_status():
     data = resp.json()
     if data[0]['status'] == 1:
       status = True
-    power_on_srv = rospy.ServiceProxy('/thermostat/power_on', SetBool)
-    resp = power_on_srv(status)
+    #power_on_srv = rospy.ServiceProxy('/thermostat/power_on', SetBool)
+    #resp = power_on_srv(status)
     return
   except rospy.ServiceException, e:
     print "Service call failed: %s"%e
