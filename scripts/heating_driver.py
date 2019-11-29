ef3f1a400d8df19ba884e4357059526da13fbc16
jginesclavero <jginesclavero@gmail.com>
2019-11-29 20:02:12 +0100

    Fix pin number

--- scripts/heating_driver.py
+++ scripts/heating_driver.py
@@ -8,11 +8,11 @@ def callback(req):
   io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
   # establecemos el modo de operacion del pin, en este caso es de salida
   # hay que utilizar la nomenclatura de Wiring pi, es MUY IMPORTANTE
-  io.pinMode(11, io.OUTPUT)
+  io.pinMode(0, io.OUTPUT)
   if req.data:
-    io.digitalWrite(11, io.HIGH)
+    io.digitalWrite(0, io.HIGH)
   else:
-    io.digitalWrite(11, io.LOW)
+    io.digitalWrite(0, io.LOW)
 
   return SetBoolResponse()
 
