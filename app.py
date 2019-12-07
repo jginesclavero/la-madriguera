4a0248589136285664700e9885806eb4431458d2
jginesclavero <jginesclavero@gmail.com>
2019-12-07 18:52:22 +0100

    Rm signup

--- app.py
+++ app.py
@@ -58,32 +58,15 @@ def login_post():
     flash('Please check your login details and try again.')
     return redirect(url_for('login')) # if user doesn't exist or password is wrong, reload the page
 
+  user = User.query.filter_by(username=username).first()
   # if the above check passes, then we know the user has the right credentials
   login_user(user, remember=remember)
   return redirect(url_for('heating_system'))
 
 @app.route('/signup')
 def signup():
-  return render_template('signup.html')
-
-@app.route('/signup', methods=['POST'])
-def signup_post():
-  username = request.form.get('username')
-  password = request.form.get('password')
-
-  user = User.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
-
-  if user: # if a user is found, we want to redirect back to signup page so user can try again
-    flash('Username already exists')
-    return redirect(url_for('signup'))
-
-  # create new user with the form data. Hash the password so plaintext version isn't saved.
-  new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
-  
-  # add the new user to the database
-  db.session.add(new_user)
-  db.session.commit()
-  return redirect(url_for('login'))
+  #return render_template('signup.html')
+  return "Under construction..."
 
 @app.route('/logout')
 @login_required
