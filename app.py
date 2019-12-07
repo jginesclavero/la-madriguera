92686f7ef17ad87aa070bc9aa1f7a09115beb8c7
jginesclavero <jginesclavero@gmail.com>
2019-12-07 18:27:53 +0100

    Add user login

--- app.py
+++ app.py
@@ -1,16 +1,96 @@
 # app.py
-from flask import Flask, request, jsonify, render_template
+from flask import Blueprint, Flask, request, jsonify, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 
 app = Flask(__name__)
-system_up = False
-
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import SystemStatus
+from models import SystemStatus, User
+
+main = Blueprint('main', __name__)
+auth = Blueprint('auth', __name__)
+
+app.register_blueprint(main)
+app.register_blueprint(auth)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+  # since the user_id is just the primary key of our user table, use it in the query for the user
+  return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/heating-system')
+@login_required
+def heating_system():
+  return render_template("heating-system.html")
+
+@app.route('/login')
+def login():
+  return render_template('login.html') 
+
+def check_login(username, password):
+  user = User.query.filter_by(username=username).first()
+  if not user or not check_password_hash(user.password, password): 
+    return False
+  return True
+
+@app.route('/login', methods=['POST'])
+def login_post():
+  username = request.form.get('username')
+  password = request.form.get('password')
+  remember = True if request.form.get('remember') else False
+
+  # check if user actually exists
+  # take the user supplied password, hash it, and compare it to the hashed password in database
+  if not check_login(username, password): 
+    flash('Please check your login details and try again.')
+    return redirect(url_for('login')) # if user doesn't exist or password is wrong, reload the page
+
+  # if the above check passes, then we know the user has the right credentials
+  login_user(user, remember=remember)
+  return redirect(url_for('heating_system'))
+
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+  username = request.form.get('username')
+  password = request.form.get('password')
+
+  user = User.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
+
+  if user: # if a user is found, we want to redirect back to signup page so user can try again
+    flash('Username already exists')
+    return redirect(url_for('signup'))
+
+  # create new user with the form data. Hash the password so plaintext version isn't saved.
+  new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+  
+  # add the new user to the database
+  db.session.add(new_user)
+  db.session.commit()
+  return redirect(url_for('login'))
+
+@app.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for('index'))
+
 
 @app.route('/heating-system/getStatus', methods=['GET'])
 def respond():
@@ -23,7 +103,14 @@ def respond():
 @app.route("/heating-system/setTemp")
 def set_temp():
   name = "heating system"
+
   temp = request.args.get('temp')
+  username = request.args.get('username')
+  password = request.args.get('password')
+
+  if not check_login(username, password): 
+    return "Loggin required"
+
   user_query = SystemStatus.query.filter_by(name='heating system')
   if user_query.count() > 0:
     user_query.update({SystemStatus.temp:temp}, synchronize_session = False)
@@ -47,6 +134,13 @@ def set_temp():
 def set_status():
   name = "heating system"
   status = request.args.get('status')
+
+  username = request.args.get('username')
+  password = request.args.get('password')
+
+  if not check_login(username, password): 
+    return "Loggin required"
+
   user_query = SystemStatus.query.filter_by(name='heating system')
   if user_query.count() > 0:
     user_query.update({SystemStatus.status:status}, synchronize_session = False)
@@ -67,18 +161,6 @@ def set_status():
 	    return(str(e))
 
    
-@app.route('/heating-system')
-def heating_system_index():
-  return render_template("heating-system-set-status.html")
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-  return "<h1>Welcome to our server !!</h1>"
-
 if __name__ == '__main__':
     app.run()
 
-#if __name__ == '__main__':
-#    # Threaded option to enable multiple instances for multiple user access support
-#    app.run(threaded=True, port=5000)
--- models.py
+++ models.py
@@ -1,25 +1,49 @@
 from app import db
+from random import randrange
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+  username = db.Column(db.String(100), unique=True)
+  password = db.Column(db.String(100))
+
+  def __init__(self, username, password):
+    self.id = randrange(100)
+    self.username = username
+    self.password = password
+
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
+    
+  def serialize(self):
+    return {
+      'id': self.id, 
+      'username': self.username,
+      'password': self.password
+    }
 
 class SystemStatus(db.Model):
-    __tablename__ = 'system_status'
+  __tablename__ = 'system_status'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    status = db.Column(db.Integer)
-    temp = db.Column(db.Integer)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String())
+  status = db.Column(db.Integer)
+  temp = db.Column(db.Integer)
     
-    def __init__(self, id, name, status, temp):
-        self.name = name
-        self.status = status
-        self.id = id
-        self.temp = temp
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+  def __init__(self, id, name, status, temp):
+    self.name = name
+    self.status = status
+    self.id = id
+    self.temp = temp
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
     
-    def serialize(self):
-        return {
-            'id': self.id, 
-            'name': self.name,
-            'status': self.status,
-            'temp': self.temp
-        }
+  def serialize(self):
+    return {
+      'id': self.id, 
+      'name': self.name,
+      'status': self.status,
+      'temp': self.temp
+    }
